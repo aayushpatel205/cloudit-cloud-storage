b001db44004cebd49d2bb3d39fd94d53610f6fa8
Aayush <aayush2052005@gmail.com>
2025-08-10 16:24:53 +0530

    fixed route

--- app/api/trash/auto-delete/route.js
+++ app/api/trash/auto-delete/route.js
@@ -1,11 +1,5 @@
-// app/api/trash/auto-delete/route.cron.js
-
 import deleteOldTrash from "@/service/DeleteOldTrash";
 import { NextResponse } from "next/server";
-
-export const config = {
-  schedule: "30 18 * * *" // 00:00 IST daily
-};
 
 export async function GET() {
   try {
