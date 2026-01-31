db6d0fcba644e199b41b1b66f57df475c2db7dba
Aayush Patel <161136244+aayushpatel205@users.noreply.github.com>
2026-01-31 11:11:44 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,7 +1,6 @@
 import dotenv from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
 dotenv.config();
-console.log(process.env.NEXT_PUBLIC_DATABASE_URL);
 
 if(!process.env.NEXT_PUBLIC_DATABASE_URL) {
   throw new Error('DATABASE_URL is not defined');
