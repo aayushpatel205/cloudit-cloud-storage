79397ea14a8b017bb6325130f2b25cca300dbbbe
Aayush <aayush2052005@gmail.com>
2026-02-04 18:49:04 +0530

    changed config

--- hooks/useUserFiles.js
+++ hooks/useUserFiles.js
@@ -2,17 +2,15 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchUserFiles } from "@/service/fetchUserFiles";
 
-
-// hooks/useUserFiles.ts
 export const useUserFiles = ({ userId, parentId }) => {
   return useQuery({
     queryKey: ["files", userId, parentId],
     queryFn: () => fetchUserFiles({ userId, parentId}),
     enabled: !!userId,
     staleTime: 5 * 60_000,
-    cacheTime: 10 * 60_000,
+    gcTime: 10 * 60_000,
     keepPreviousData: true,
-    placeholderData: (previousData) => previousData, // 🔥 KEY LINE
+    placeholderData: (previousData) => previousData
   });
 };
 
